62d908645d7e1e04f2d03e3149ae74a80582687e
Dustin Hagstrom <dustnhgstrm@gmail.com>
2023-12-17 15:33:09 -0600

    wrote the database setup and queries that will be used by the app.

--- database.sql
+++ database.sql
@@ -1,13 +1,48 @@
+-- drop table
 DROP TABLE IF EXISTS "todos";
 
+-- create table
 CREATE TABLE "todos" (
-	"id" SERIAL PRIMARY KEY,
-	"text" TEXT,
-	"isComplete" BOOLEAN DEFAULT FALSE
+"id" SERIAL PRIMARY KEY,
+"text" TEXT,
+"isComplete" BOOL DEFAULT FALSE
 );
+
 
 INSERT INTO "todos"
   ("text")
   VALUES 
   ('Build a CRUD app'),
   ('Make my app look nice');
+
+-- Get all data
+SELECT * FROM "todos";
+
+-- Create a todo
+INSERT INTO "todos"
+  ("text")
+  VALUES
+  ($1);
+
+-- Example INSERT query
+INSERT INTO "todos"
+  ("text")
+  VALUES
+  ('This is a brand new todo');
+
+-- Toggle isComplete Boolean with Update query
+UPDATE "todos" SET "isComplete" = Not "isComplete" 
+WHERE "id" = $1;
+
+-- Example UPDATE query
+UPDATE "todos" SET "isComplete" = Not "isComplete" 
+WHERE "id" = 2;
+
+-- Delete table entry by id
+DELETE FROM "todos"
+WHERE "id" = $1;
+
+-- Example DELETE query
+DELETE FROM "todos"
+WHERE "id" = 2;
+
